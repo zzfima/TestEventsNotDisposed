5fdd709b0ae5892446674887260f3a6f5a418854
zzfima <zzfima@gmail.com>
2024-05-26 20:45:22 +0300

    Add project files.

---
+++ TestEventsNotDisposed/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace TestEventsNotDisposed
+{
+    public class Publisher
+    {
+        public event EventHandler SomeEventHappens;
+
+        public void SomeFunc()
+        {
+            SomeEventHappens?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public class Subscriber
+    {
+        private Publisher _publisher;
+        private int _id;
+
+        public Subscriber(Publisher publisher, int id)
+        {
+            _publisher = publisher;
+            _id = id;
+            _publisher.SomeEventHappens += SomeEventHappens;
+        }
+
+        private void SomeEventHappens(object sender, EventArgs e)
+        {
+            Console.WriteLine($"Subscriber {_id} handling Elapsed event.");
+        }
+
+        ~Subscriber()
+        {
+            Console.WriteLine($"Subscriber {_id} finalized.");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var publisher = new Publisher();
+
+            for (int i = 1; i <= 113; i++)
+            {
+                var subscriber = new Subscriber(publisher, i);
+                Console.WriteLine($"Subscriber {i} created. Press Enter to create next subscriber...");
+                Console.ReadLine();
+                publisher.SomeFunc();
+            }
+
+            Console.WriteLine("Press Enter to exit and see remaining notifications...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TestEventstDisposed/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace TestEventsDisposed
+{
+    public class Publisher
+    {
+        public event EventHandler SomeEventHappens;
+
+        public void SomeFunc()
+        {
+            SomeEventHappens?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public class Subscriber : IDisposable
+    {
+        private Publisher _publisher;
+        private int _id;
+        private bool _disposed = false;
+
+        public Subscriber(Publisher publisher, int id)
+        {
+            _publisher = publisher;
+            _id = id;
+            _publisher.SomeEventHappens += SomeEventHappens;
+        }
+
+        private void SomeEventHappens(object sender, EventArgs e)
+        {
+            Console.WriteLine($"Subscriber {_id} handling Elapsed event.");
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~Subscriber()
+        {
+            Console.WriteLine($"Subscriber {_id} finalized.");
+            Dispose(false);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                Console.WriteLine($"Subscriber {_id} disposed.");
+                if (disposing)
+                    _publisher.SomeEventHappens -= SomeEventHappens;
+
+                _disposed = true;
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var publisher = new Publisher();
+
+            for (int i = 1; i <= 113; i++)
+            {
+                using (var subscriber = new Subscriber(publisher, i))
+                {
+                    Console.WriteLine($"Subscriber {i} created. Press Enter to create next subscriber...");
+                    Console.ReadLine();
+                    publisher.SomeFunc();
+                }
+            }
+
+            Console.WriteLine("Press Enter to exit and see remaining notifications...");
+            Console.ReadLine();
+        }
+    }
+}
